b175c17ecc7ba111eb57887dc4d400688ca6110e
tanneratkin <tmanatkin@gmail.com>
2025-07-11 11:21:51 -0600

    refined styling, split script for seperate build processes. updated markdown

---
+++ src/scripts/build-html.mjs
@@ -0,0 +1,49 @@
+import fs from "fs";
+import chokidar from "chokidar";
+import * as sass from "sass";
+import handlebars from "handlebars";
+import markdownIt from "markdown-it";
+import container from "markdown-it-container";
+
+// single build on start
+await build();
+console.log("HTML build complete.");
+
+// constant auto-build using watcher
+if (process.argv.includes("--auto")) {
+  const watcher = chokidar.watch("./src", {
+    ignoreInitial: true,
+    ignored: /src\/scripts/ // ignore all script files
+  });
+
+  watcher.on("change", async () => {
+    await build();
+    console.log("Changes saved and built.");
+    console.log("Watching for changes...");
+  });
+
+  console.log("Watching for changes...");
+}
+
+// main build function
+async function build() {
+  const resumeFile = fs.readFileSync("./src/markdown/resume.md", "utf8");
+  const templateFile = fs.readFileSync("./src/template.hbs", "utf8");
+
+  // create markdown-it instance
+  const md = new markdownIt();
+  md.use(container, "title-date-container"); // add title-date-container container
+
+  // compile SCSS
+  const styles = sass.compile("./src/scss/main.scss", {
+    style: "compressed"
+  });
+
+  // render markdown to HTML
+  const body = md.render(resumeFile);
+
+  // write HTML to file using template
+  const template = handlebars.compile(templateFile);
+  const html = template({ body, styles: styles.css });
+  await fs.promises.writeFile("./output/resume.html", html);
+}
